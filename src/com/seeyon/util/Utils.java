c97ca0e0318d8e2f72c5f4f288fbeea1d8fcb01c
renjb <751288533@qq.com>
2021-01-13 10:16:22 +0800

    第一次提交git文件

---
+++ src/com/seeyon/util/Utils.java
@@ -0,0 +1,13 @@
+package com.seeyon.util;
+
+/**
+ * @Auther: Jbin
+ * @Date: 2021/1/13 - 01 - 13
+ * @Description: com.seeyon.util
+ * @version: 1.0
+ */
+public class Utils {
+    public static void main(String[] args) {
+        System.out.println("hi testGit!");
+    }
+}
